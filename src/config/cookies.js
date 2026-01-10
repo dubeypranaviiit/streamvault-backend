8a3f214cbedfb684162291c124cae9f084a92ab6
Pranav Abhimanyu <146646970+dubeypranaviiit@users.noreply.github.com>
2026-01-10 22:21:13 +0530

    Update cookies.js

--- src/config/cookies.js
+++ src/config/cookies.js
@@ -7,7 +7,7 @@ export const accessTokenCookieOptions = {
 };
 export const refreshTokenCookieOptions = {
   httpOnly: true,
-  secure:true",
+  secure:true,
   sameSite: "none",
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
