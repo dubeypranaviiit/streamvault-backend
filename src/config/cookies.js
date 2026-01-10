a5d121af5480b155432c36275ea8a96175afea72
Pranav Abhimanyu <146646970+dubeypranaviiit@users.noreply.github.com>
2026-01-10 22:15:27 +0530

    Update cookies.js

--- src/config/cookies.js
+++ src/config/cookies.js
@@ -1,13 +1,13 @@
 import envFile from "./env.js";
 export const accessTokenCookieOptions = {
   httpOnly: true,
-  secure: envFile.NODE_ENV === "production",
-  sameSite: "strict",
+  secure:true,
+  sameSite: "none",
   maxAge: 15 * 60 * 1000,
 };
 export const refreshTokenCookieOptions = {
   httpOnly: true,
-  secure: envFile.NODE_ENV === "production",
-  sameSite: "strict",
+  secure:true",
+  sameSite: "none",
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
