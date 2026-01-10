73467fc50caa78acc2b19abe7ff85b01f2f86229
Pranav Abhimanyu <146646970+dubeypranaviiit@users.noreply.github.com>
2026-01-10 21:02:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,21 +10,27 @@ import cookieParser from "cookie-parser";
 import adminRoutes from "./src/routes/adminRoutes.js";
 const app = express();
 const server = http.createServer(app);
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://streamvault-frontend.vercel.app",
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173", 
-    credentials: true,             
+    origin: allowedOrigins,
+    credentials: true,
   })
 );
-app.use(express.json());
-app.use(cookieParser()); 
+
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
-    credentials: true, 
+    origin: allowedOrigins,
+    credentials: true,
   },
 });
 
+app.use(express.json());
+app.use(cookieParser()); 
 app.use((req, res, next) => {
   req.io = io;
   next();
